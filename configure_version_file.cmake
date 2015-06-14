04022e3df935f382e0d7f080f7b9feffd6cab946
Paul <Paul@paul.com>
2015-06-14 20:05:36 +0100

    rework versioning

---
+++ configure_version_file.cmake
@@ -0,0 +1,4 @@
+set(SRC_DIR "C:/Users/paul/Desktop/alive/alive")
+set(BIN_DIR "C:/Users/paul/Desktop/alive/alive/build")
+set(ALIVE_VERSION "ALIVE v0.0.0 rev(a8212fc)")
+configure_file(C:/Users/paul/Desktop/alive/alive/include/alive_version.h.in /alive_version.h @ONLY)
