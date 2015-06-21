02ceca9fa901c39e54711d857b18996c2963c35b
Paul <Paul@paul.com>
2015-06-21 16:21:47 +0100

    Move CMake scripts to a dedicated directory

--- configure_version_file.cmake
+++
@@ -1,4 +0,0 @@
-set(SRC_DIR "C:/Users/paul/Desktop/alive/alive")
-set(BIN_DIR "C:/Users/paul/Desktop/alive/alive/build")
-set(ALIVE_VERSION "ALIVE v0.0.0 rev(a8212fc)")
-configure_file(C:/Users/paul/Desktop/alive/alive/include/alive_version.h.in /alive_version.h @ONLY)
