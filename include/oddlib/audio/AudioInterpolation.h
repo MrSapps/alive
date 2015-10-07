fe0823cf1dde62b2131338770fb27cbb25ac511f
Tuukka Pensala <tuukka.pensala@gmail.com>
2015-10-07 20:01:47 +0300

    Some audio output tests and options (antialiasing, cubic interpolation)

---
+++ include/oddlib/audio/AudioInterpolation.h
@@ -0,0 +1,7 @@
+#pragma once
+
+enum AudioInterpolation {
+	AudioInterpolation_none,
+	AudioInterpolation_linear, // Causes digitized feel when lowering frequency. Also noise.
+	AudioInterpolation_cubic, // Cause hight ringing, which can be removed by filtering.
+};
