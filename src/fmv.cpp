1d2cc604d53cd0681fc8cc37a07bd360ec8a0fb2
Paul <Paul@paul.com>
2015-10-09 15:44:11 +0100

    don't seem to need the subtitle audio lag fix anymore

--- src/fmv.cpp
+++ src/fmv.cpp
@@ -125,7 +125,7 @@ public:
         if (mSubTitles)
         {
             // We assume the FPS is always 15, thus 1000/15=66.66 so frame number * 66 = number of msecs into the video
-            const auto& subs = mSubTitles->Find((videoFrameIndex * 66)+500); // audio has about 500msec latency, so fix it up to sync the subs
+            const auto& subs = mSubTitles->Find((videoFrameIndex * 66));
             if (!subs.empty())
             {
                 // TODO: Render all active subs, not just the first one
