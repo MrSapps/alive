6c49a06f1076d2f2ae77cc444c0e62d1822fd2bc
Paul <Paul@paul.com>
2015-10-21 21:44:26 +0100

    try again

--- set_compiler.bat
+++ set_compiler.bat
@@ -1,40 +1,41 @@
-:: Now we declare a scope
-Setlocal EnableDelayedExpansion EnableExtensions
-
-echo %compiler%
+@echo off
 
 if not defined compiler set compiler=2015
-
-if "%compiler%"=="MinGW" ( goto :mingw )
+echo compiler is: %compiler%
 
 if "%compiler%"=="2015" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %platform%
+    goto :eof
 )
 
 if "%compiler%"=="2013" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+    echo "Set 2013"
+    call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %platform%
+    goto :eof
 )
 
 if "%compiler%"=="2012" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+    echo "Set 2012"
+    call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" %platform%
+    goto :eof
 )
 
 if "%compiler%"=="2010" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+    echo "Set 2010"
+    call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" %platform%
+    goto :eof
 )
 
 if "%compiler%"=="2008" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+    echo "Set 2008"
+    call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" %platform%
+    goto :eof
 )
 
-:: Visual Studio detected
-endlocal
-echo %SET_VS_ENV%
-echo "set vsvars"
-call %SET_VS_ENV% %platform%
-goto :eof
+if "%compiler%"=="MinGW"  ( 
+    echo "Set MinGW"
+    set PATH=c:\mingw\bin;%PATH%
+    goto :eof
+)
 
-:: MinGW detected
-:mingw
-echo "set minGW vars"
-endlocal & set PATH=c:\mingw\bin;%PATH%
+:eof
