e288ab7170254c2738166fecd312bf9b2214c227
Paul <Paul@paul.com>
2016-05-25 23:12:08 +0100

    current

--- include/zipfilesystem.hpp
+++ include/zipfilesystem.hpp
@@ -3,6 +3,7 @@
 #include "resourcemapper.hpp"
 #include "unzip.h"
 #include "ioapi.h"
+#include "iowin32.h"
 
 // Actually "ZIP64" file system, which removes 65k file limit and 4GB zip file size limit
 class ZipFileSystem
@@ -10,9 +11,21 @@ class ZipFileSystem
 public:
     ZipFileSystem()
     {
-        zlib_filefunc_def filefunc32 = { 0 };
+        zlib_filefunc64_def filefunc32 = { };
 
+        //fill_win32_filefunc64(&filefunc32);
+        fill_fopen64_filefunc(&filefunc32);
 
-        unzOpen2("__notused__", &filefunc32);
+        unzFile  f = unzOpen2_64("__notused__", &filefunc32);
+        if (f)
+        {
+            unz_file_info64 fileInfo = {};
+            unzGetCurrentFileInfo64(f, &fileInfo, "test.txt", 0, 0, 0, 0, 0);
+
+            char buffer[10];
+            unzReadCurrentFile(f, buffer, 10);
+
+            unzClose(f);
+        }
     }
 };
