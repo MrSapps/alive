3c6518fc08b6e47c37aa917ad251c69b80a5ad00
Paul <Paul@paul.com>
2017-08-11 23:43:02 +0100

    add list box to ui

--- tools/engine_hook/debug_dialog.cpp
+++ tools/engine_hook/debug_dialog.cpp
@@ -1,31 +1,81 @@
 #include "debug_dialog.hpp"
+#include "resource.h"
 
 extern HMODULE gDllHandle;
 
-BOOL CALLBACK DlgProc(HWND /*hwnd*/, UINT message, WPARAM /*wParam*/, LPARAM /*lParam*/)
+class ListBox : public BaseWindow
 {
-    switch (message)
+public:
+    explicit ListBox(HWND hwnd)
     {
-    case WM_INITDIALOG:
-        return FALSE;
-
-    default:
-        return FALSE;
+        mHwnd = hwnd;
     }
-}
 
-bool DebugDialog::Create(LPCSTR dialogId)
-{
-    mHwnd = ::CreateDialog(gDllHandle, dialogId, NULL, DlgProc);
-    return mHwnd != NULL;
-}
+    void AddString(const char* str)
+    {
+        ::SendMessage(mHwnd, LB_ADDSTRING, 0, reinterpret_cast<WPARAM>(str));
+    }
+};
 
-void DebugDialog::Show()
+void BaseWindow::Show()
 {
     ::ShowWindow(mHwnd, SW_SHOW);
 }
 
-void DebugDialog::Destroy()
+void BaseWindow::Destroy()
 {
     ::DestroyWindow(mHwnd);
+}
+
+BaseWindow::~BaseWindow()
+{
+
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    if (message == WM_CLOSE)
+    {
+        SetWindowLongPtrW(hwnd, GWLP_USERDATA, 0);
+        PostQuitMessage(0);
+        return TRUE;
+    }
+
+    DebugDialog* thisPtr = (DebugDialog*)GetWindowLongPtr(hwnd, GWL_USERDATA);
+    if (thisPtr)
+    {
+        return thisPtr->Proc(hwnd, message, wParam, lParam);
+    }
+
+    if (message == WM_INITDIALOG)
+    {
+        SetWindowLongPtr(hwnd, GWL_USERDATA, (LONG_PTR)lParam);
+        thisPtr = (DebugDialog*)GetWindowLongPtr(hwnd, GWL_USERDATA);
+        return thisPtr->Proc(hwnd, message, wParam, lParam);
+    }
+
+    return FALSE;
+}
+
+bool DebugDialog::Create(LPCSTR dialogId)
+{
+    mHwnd = ::CreateDialogParam(gDllHandle, dialogId, NULL, DlgProc, reinterpret_cast<LPARAM>(this));
+    return mHwnd != NULL;
+}
+
+BOOL DebugDialog::Proc(HWND hwnd, UINT message, WPARAM /*wParam*/, LPARAM /*lParam*/)
+{
+    switch (message)
+    {
+    case WM_INITDIALOG:
+    {
+        HWND hListBox = GetDlgItem(hwnd, IDC_ANIMATIONS);
+        auto listbox = std::make_unique<ListBox>(hListBox);
+        listbox->AddString("Testing");
+        mWindows.push_back(std::move(listbox));
+    }
+    return FALSE;
+    }
+
+    return FALSE;
 }
--- tools/engine_hook/debug_dialog.hpp
+++ tools/engine_hook/debug_dialog.hpp
@@ -1,15 +1,26 @@
 #pragma once
 
 #include <windows.h>
+#include <memory>
+#include <vector>
 
-class DebugDialog
+class BaseWindow
+{
+public:
+    HWND Hwnd() const { return mHwnd; }
+    void Show();
+    void Destroy();
+    virtual ~BaseWindow();
+protected:
+    HWND mHwnd = NULL;
+};
+using UP_BaseWindow = std::unique_ptr<BaseWindow>;
+
+class DebugDialog : public BaseWindow
 {
 public:
     bool Create(LPCSTR dialogId);
-    void Show();
-    HWND Hwnd() const { return mHwnd; }
+    BOOL Proc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 private:
-    HWND mHwnd = NULL;
-public:
-    void Destroy();
+    std::vector<UP_BaseWindow> mWindows;
 };
