f24197666b8b5c7ddc14369a566fe00f35b1640f
Paul <Paul@paul.com>
2017-10-24 22:43:10 +0100

    testing menu byte code idea

--- src/gamemode.cpp
+++ src/gamemode.cpp
@@ -5,12 +5,196 @@
 #include "gridmap.hpp"
 #include "fmv.hpp"
 
+enum class ByteCodeInstruction : u8
+{
+    kNumberLiteral = 0x1
+};
+
+class InPlaceReadOnlyMemoryStream
+{
+public:
+    bool AtEnd() { return false; }
+    u8 ReadByte() { return 0; }
+private:
+    std::vector<u8> mData;
+};
+
+class VirtualMachine
+{
+public:
+    void Execute()
+    {
+        while (!mByteCode.AtEnd())
+        {
+            ExecuteInstruction(mByteCode.ReadByte());
+        }
+    }
+
+    void ExecuteInstruction(u8 instruction)
+    {
+        switch (instruction)
+        {
+        case ByteCodeInstruction::kNumberLiteral:
+            Push(mByteCode.ReadByte());
+            break;
+        }
+    }
+
+    void Push(s32 value)
+    {
+        mStack[mStackPos++] = value;
+    }
+
+    s32 Pop()
+    {
+        return mStack[--mStackPos];
+    }
+private:
+    s32 mStackPos = 0;
+    s32 mStack[256] = {};
+
+    InPlaceReadOnlyMemoryStream mByteCode;
+};
+
+class ByteCodeEmitter
+{
+public:
+    ByteCodeEmitter()
+    {
+
+    }
+
+    void StartingScreen(const std::string&)
+    {
+
+    }
+
+    void ChangeScreen(const std::string&)
+    {
+
+    }
+
+    void OnEnter()
+    {
+
+    }
+
+    void OnExit()
+    {
+
+    }
+
+    void Delay(s32)
+    {
+
+    }
+
+    void IdleTimeOut(s32)
+    {
+
+    }
+
+    void PlayFmv(const std::string&)
+    {
+
+    }
+
+    void PlayAnimation(const std::string&)
+    {
+
+    }
+
+
+    void BeginOnExit(const std::string&)
+    {
+        throw std::logic_error("The method or operation is not implemented.");
+    }
+
+
+
+    void EndOnExit()
+    {
+        throw std::logic_error("The method or operation is not implemented.");
+    }
+
+
+
+    void BeginOnEnter(const std::string&)
+    {
+        throw std::logic_error("The method or operation is not implemented.");
+    }
+
+
+
+    void EndOnEnter()
+    {
+        throw std::logic_error("The method or operation is not implemented.");
+    }
+
+
+
+    void DisableMenu()
+    {
+        throw std::logic_error("The method or operation is not implemented.");
+    }
+
+
+
+    void EnableMenu()
+    {
+        throw std::logic_error("The method or operation is not implemented.");
+    }
+
+
+private:
+    void Write(s32)
+    {
+
+    }
+
+    void Write(const std::string&)
+    {
+
+    }
+
+    //Oddlib::MemoryStream mStream;
+};
+
 GameMode::GameMode(WorldState& mapState)
     : mWorldState(mapState)
 {
     // hack/wip/test
-    mState = eMenu;
+    //mState = eMenu;
 
+}
+
+void Test()
+{
+    ByteCodeEmitter be;
+
+    be.StartingScreen("STP01C25.CAM");
+    be.Delay(100);
+
+    be.BeginOnExit("STP01C25.CAM");
+    be.DisableMenu();
+    be.EndOnEnter();
+
+    be.BeginOnExit("STP01C25.CAM");
+    be.PlayFmv("AE_GTILogo");
+    be.PlayFmv("AE_DigitalDialect");
+    be.PlayFmv("AE_OWILogo");
+    be.PlayFmv("AE_Intro");
+    be.PlayFmv("AE_MainMenu");
+    be.ChangeScreen("STP01C01.CAM");
+
+    be.EndOnExit();
+
+    be.BeginOnEnter("STP01C01.CAM");
+    be.PlayAnimation("StDoorOpen");
+    be.PlayAnimation("AbeHello");
+    be.EnableMenu();
+
+    be.EndOnEnter();
 }
 
 void GameMode::UpdateMenu(const InputState& /*input*/, CoordinateSpace& /*coords*/)
